1bb7cc70b2a87570e9db36895f8340b088449685
Alexandr Tichon <tsikhan.p@gmail.com>
2019-04-30 23:31:41 +0300

    feat: color picker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import initCanvas from './api/init';
 
 const canvasContainer = document.querySelector('#canvas');
@@ -5,14 +6,16 @@ const backetBtn = document.querySelector('#backet-tool');
 const colorPickerBtn = document.querySelector('#color-picker-tool');
 const moveBtn = document.querySelector('#move-tool');
 const transformBtn = document.querySelector('#transform-tool');
-const currentColorBtn = document.querySelector('#current-color');
-const prevColorBtn = document.querySelector('#prev-color');
-const predefinedColorOne = document.querySelector('#predefined-color1');
-const predefinedColorTwo = document.querySelector('#predefined-color2');
+const currentColor = document.querySelector('#color-block-current');
+const prevColor = document.querySelector('#prev-color');
+const predefinedColorOne = document.querySelector('#color-block-predefined1');
+const predefinedColorTwo = document.querySelector('#color-block-predefined2');
 
 const elementsInterval = 10;
 
 // initCanvas(canvasContainer);
+
+// init canvas
 
 let figureCounter = 0;
 
@@ -37,3 +40,67 @@ circle.className = 'circle-element';
 circle.style.top = '470px';
 circle.style.left = '0px';
 canvasContainer.appendChild(circle);
+
+const colorList = ['000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333',
+  '660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333', 'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33', 'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC', 'FFCC99', 'FFFF99', 'CCffCC', 'CCFFff', '99CCFF', 'CC99FF', 'FFFFFF'];
+const picker = document.querySelector('#color-block-picker');
+
+for (let i = 0; i < colorList.length; i += 1) {
+  const li = document.createElement('li');
+  li.className = 'color-item';
+  li.style.backgroundColor = `#${colorList[i]}`;
+  li.dataset.hex = `#${colorList[i]}`;
+  picker.appendChild(li);
+}
+for (let i = 0; i < colorList.length; i += 1) {
+  const li = document.createElement('li');
+  li.className = 'color-item';
+  li.style.backgroundColor = `#${colorList[i]}`;
+  li.dataset.hex = `#${colorList[i]}`;
+  predefinedColorOne.appendChild(li);
+}
+for (let i = 0; i < colorList.length; i += 1) {
+  const li = document.createElement('li');
+  li.className = 'color-item';
+  li.style.backgroundColor = `#${colorList[i]}`;
+  li.dataset.hex = `#${colorList[i]}`;
+  predefinedColorTwo.appendChild(li);
+}
+
+$('body').click(() => {
+  $('#color-block-picker').fadeOut();
+  $('#color-block-predefined1').fadeOut();
+  $('#color-block-predefined2').fadeOut();
+});
+
+document.querySelectorAll('.call-picker');
+
+$('.call-picker').click((event) => {
+  event.stopPropagation();
+  $('#color-block-picker').fadeIn();
+  $('#color-block-picker').children('li').hover((element) => {
+    const codeHex = element.currentTarget.dataset.hex;
+    $('#color-picker-holder').css('background-color', codeHex);
+    $('#color-current-holder').css('background-color', codeHex);
+  });
+});
+
+$('#color-predefined1-holder').click((event) => {
+  event.stopPropagation();
+  $('#color-block-predefined1').fadeIn();
+  $('#color-block-predefined1').children('li').hover((element) => {
+    const codeHex = element.currentTarget.dataset.hex;
+    $('#color-predefined1-holder').css('background-color', codeHex);
+  });
+});
+
+$('#color-predefined2-holder').click((event) => {
+  event.stopPropagation();
+  $('#color-block-predefined2').fadeIn();
+  $('#color-block-predefined2').children('li').hover((element) => {
+    const codeHex = element.currentTarget.dataset.hex;
+    $('#color-predefined2-holder').css('background-color', codeHex);
+  });
+});
+
+backetEvent = new Event();
